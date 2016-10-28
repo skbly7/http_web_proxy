fdd30e14111c26176f459bcbd12f2fd92db69625
Shivam Khandelwal <skbly7@gmail.com>
2016-10-29 02:49:29 +0530

    Fixed parse error coming on specifying listen address

--- src/run.py
+++ src/run.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         port = int(sys.argv[1])
     listen_address = "127.0.0.1"
     if len(sys.argv) > 2:
-        listen_address = int(sys.argv[2])
+        listen_address = sys.argv[2]
     print "Listening on " + str(port) + " ..."
     server = WebProxyServer(port=port, listen_address=listen_address, debug=True, cache=True)
     # server.set_parent_proxy("proxy.iiit.ac.in", 8080)
